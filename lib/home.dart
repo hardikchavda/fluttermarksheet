aa2647c0a33fae49aad4141f6b44fbef86011c95
Hardik Chavda <hardikkchavda@gmail.com>
2022-04-15 12:22:35 +0530

    Navigation Drawer Updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/marksheet.dart';
-
-void main() => runApp(const HomeApp());
+import 'package:myapp/navigatiodrawer.dart';
+import 'package:myapp/pageroutes.dart';
 
 class HomeApp extends StatelessWidget {
   const HomeApp({Key? key}) : super(key: key);
+  static const String routeName = 'HomeApp';
 
   static const appTitle = 'Drawer Demo';
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: appTitle,
-      home: MyHomePage(title: appTitle),
+      home: const MyHomePage(title: appTitle),
+      routes: {
+        pageroutes.home: (context) => const HomeApp(),
+        pageroutes.marksheet: (context) => const MarkSheet(),
+      },
     );
   }
 }
@@ -29,38 +34,7 @@ class MyHomePage extends StatelessWidget {
       body: const Center(
         child: Text('My Page!'),
       ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                //  Navigator.pushReplacementNamed(context, ),
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: const navigationdrawer(),
     );
   }
 }
